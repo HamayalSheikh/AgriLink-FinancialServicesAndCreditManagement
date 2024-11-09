78591bbae3481d0fa18cff53c6d476c51ae5b867
mahad002 <mahad112002@gmail.com>
2024-11-09 22:43:03 +0500

    Base Project - FIrst Commit

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+const transactionRoutes = require('./routes/transactionRoutes');
+const paymentRoutes = require('./routes/paymentRoutes');
+const loanApplicationRoutes = require('./routes/loanApplicationRoutes');
+const loanRepaymentMonitoringRoutes = require('./routes/loanRepaymentMonitoringRoutes');
+
+const app = express();
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+// Route Middleware
+app.use('/users', userRoutes);
+app.use('/transactions', transactionRoutes);
+app.use('/payments', paymentRoutes);
+app.use('/loan-applications', loanApplicationRoutes);
+app.use('/loan-repayments', loanRepaymentMonitoringRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
